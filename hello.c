d9fe7f568b23aaca279c14203126e7b7b760dbdd
Gabriel <gabrielvitor.frasao@gmail.com>
2023-08-28 19:21:15 -0300

    update hello.c, ignore bin directory

--- hello.c
+++ hello.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 
 int main(void)
 {
-	int name = 11;
-	printf("hello, %i", name);
+	int count = 13;
+	for (int i = 0; i < count; i++)
+	{
+		printf("hello, %i\n", i);
+	}
 }
